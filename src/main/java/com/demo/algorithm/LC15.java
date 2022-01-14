30af39398fb218228aa3b2f689e2437c58667d4f
gnl <gnl0@qq.com>
2022-01-14 18:06:32 +0800

    update

--- src/main/java/com/demo/algorithm/LC15.java
+++ src/main/java/com/demo/algorithm/LC15.java
@@ -1,7 +1,6 @@
 package com.demo.algorithm;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 三数之和
@@ -28,16 +27,62 @@ import java.util.List;
 
 public class LC15 {
     public static void main(String[] args) {
-
+        int[] nums = {-2,0,0,2,2};
+        System.out.println(threeSum(nums));
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum(int[] nums) {
 
-        if (nums.length < 2) {
-            return Collections.emptyList();
+        List<List<Integer>> retList = new ArrayList<>();
+
+        if (nums.length < 2 || nums == null) {
+            return retList;
         }
 
-        return null;
+        // 先进行排序
+        Arrays.sort(nums);
 
+        for (int i = 0; i < nums.length; i++) {
+
+            // 对于三个数而言，如果第一个数大于0，后面的数都大于或等于它，那么三个数相加势必大于0，就没必要在继续下去了
+            if (nums[i] > 0) {
+                break;
+            }
+
+            // 去掉重复情况
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            // 第一个值
+            int first = nums[i];
+
+            // 接下来只需要关注第一个值后面的部分即可
+            int left = i + 1, right = nums.length - 1;
+
+            while (left < right) {
+                int sum = first + nums[left] + nums[right];
+                // 三数相加等于0，符合要求
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(first);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    retList.add(list);
+
+                    left++;
+                    right--;
+                } else if (sum < 0){
+                    // 三数相加小于0，移动left
+                    left++;
+                } else {
+                    // 三数相加大于0，移动right
+                    right--;
+                }
+            }
+
+        }
+        return retList;
     }
+
 }
